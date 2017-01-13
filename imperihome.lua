6dba80ec375dbc64d3bd3976c72af5e9c5787461
reneboer <git@reneboer.demon.nl>
2017-01-14 00:06:01 +0100

    V0.2
    Code rewrite now it's known to work. Added actions for Harmony Hub devices.

--- imperihome.lua
+++ imperihome.lua
@@ -3,7 +3,7 @@
 module(..., package.seeall)
 
 --[[
-Device types in ISS we can read
+Standard Vera Device types in ISS we can handle right now
 
 Cat/Sub cat	Device type string		Description
 			DevCamera 				MJPEG IP Camera 
@@ -35,15 +35,15 @@ Cat/Sub cat	Device type string		Description
 28			DevUV 					UV sensor 
 			DevWind 				Wind sensor 
 
-Curently some special handling for the Harmony Hub as MultySwitch device
+These standards can be overruled based on the device schema.
+
 ]]
-local userdata = require "openLuup.userdata"
 local luup = require "openLuup.luup"  -- Gives all Vera luup functionality
 local json = require "openLuup.json"
 
 
 -- SIDs for devices we support
-local sidMap = {
+local SIDS = {
     HA = "urn:micasaverde-com:serviceId:HaDevice1",
     Switch = "urn:upnp-org:serviceId:SwitchPower1",
     Dimmer = "urn:upnp-org:serviceId:Dimming1",
@@ -59,132 +59,158 @@ local sidMap = {
 	SM_Gas = "urn:rboer-com:serviceId:SmartMeterGAS1",
 	MSwitch = "urn:dcineco-com:serviceId:MSwitch1"
 }
--- Some special devices we do at schema level
-local schemaMap = {
+local SCHEMAS = {
+	Harmony = "urn:schemas-rboer-com:device:Harmony(%d*):1",
+	HarmonyDev = "urn:schemas-rboer-com:device:HarmonyDevice(%d*)_(%d*):1",
 	SM_Gas = "urn:schemas-rboer-com:device:SmartMeterGAS:1",
-	Harmony = "urn:schemas-rboer-com:device:Harmony",
-	HarmonyDev = "urn:schemas-rboer-com:device:HarmonyDevice",
-	MSwitch = "urn:schemas-dcineco-com:device:MSwitch"
+	MSwitch = "xxxurn:schemas-dcineco-com:device:MSwitch(%d*):1"
 }
--- The index number is the Vera Luup Device Category
-local devMap = {
-    ["2_0"] = { type = "DevDimmer", 
-			params = { 
-				Status = { sidMap.Switch, "Status" }, 
-				Level = { sidMap.Dimmer, "LoadLevelStatus" }, 
-				Energy = { sidMap.Energy, "Watts"}
-			},
-			actions = {
-				["setLevel"] = { sidMap.Dimmer, "SetLoadLevelTarget", "newLoadlevelTarget" }
-			}
-		  },
-	["3_0"] = { type = "DevSwitch", 
-			params = { 
-				Status = { sidMap.Switch, "Status" }, 
-				Energy = { sidMap.Energy, "Watts"}
-			},
-			actions = {
-				["setStatus"] = { sidMap.Switch, "SetTarget", "newTarget" }
-			}
-		   },
-	["4_0"] = { type = "DevDoor", 
-			params = { 
-				Armed = { sidMap.Sensor, "Armed" }, 
-				Tripped = { sidMap.Sensor, "Tripped"},
-				lasttrip = { sidMap.Sensor, "LastTrip"},
-				armable = "1"
-			},
-			actions = {
-				["setArmed"] = { sidMap.Sensor, "SetArmed", "newArmedValue" }
-			}
-		   },
-	["4_1"] = { type = "DevDoor", 
-			params = { 
-				Armed = { sidMap.Sensor, "Armed" }, 
-				Tripped = { sidMap.Sensor, "Tripped"},
-				lasttrip = { sidMap.Sensor, "LastTrip"},
-				armable = "1"
-			},
-			actions = {
-				["setArmed"] = { sidMap.Sensor, "SetArmed", "newArmedValue" }
-			}
-		   },
-	["4_2"] = { type = "DevFlood", 
-			params = { 
-				Armed = { sidMap.Sensor, "Armed" }, 
-				Tripped = { sidMap.Sensor, "Tripped"},
-				lasttrip = { sidMap.Sensor, "LastTrip"},
-				armable = "1"
-			},
-			actions = {
-				["setArmed"] = { sidMap.Sensor, "SetArmed", "newArmedValue" }
-			}
-		   },
-	["4_3"] = { type = "DevMotion", 
-			params = { 
-				Armed = { sidMap.Sensor, "Armed" }, 
-				Tripped = { sidMap.Sensor, "Tripped"},
-				lasttrip = { sidMap.Sensor, "LastTrip"},
-				armable = "1"
-			},
-			actions = {
-				["setArmed"] = { sidMap.Sensor, "SetArmed", "newArmedValue" }
-			}
-		   },
-	["4_4"] = { type = "DevSmoke", 
-			params = { 
-				Armed = { sidMap.Sensor, "Armed" }, 
-				Tripped = { sidMap.Sensor, "Tripped"},
-				lasttrip = { sidMap.Sensor, "LastTrip"},
-				armable = "1"
-			},
-			actions = {
-				["setArmed"] = { sidMap.Sensor, "SetArmed", "newArmedValue" }
-			}
-		   },
-	["4_5"] = { type = "DevCO2Alert", 
-			params = { 
-				Armed = { sidMap.Sensor, "Armed" }, 
-				Tripped = { sidMap.Sensor, "Tripped"},
-				lasttrip = { sidMap.Sensor, "LastTrip"},
-				armable = "1"
-			},
-			actions = {
-				["setArmed"] = { sidMap.Sensor, "SetArmed", "newArmedValue" }
-			}
-		   },
-	["12_0"] = { type = "DevGenericSensor", 
-			params = { 
-				Value = { sidMap.Generic, "CurrentLevel" }
-			}
-		   },
-	["16_0"] = { type = "DevHygrometry", 
-			params = { 
-				Value = { sidMap.Humidity, "CurrentLevel" }
-			}
-		   },
-	["17_0"] = { type = "DevTemperature", 
-			params = { 
-				Value = { sidMap.Temp, "CurrentTemperature" }
-			}
-		   },
-	["18_0"] = { type = "DevLuminosity", 
-			params = { 
-				Value = { sidMap.Light, "CurrentLevel" }
-			}
-		   },
-	["21_0"] = { type = "DevElectricity", 
-			params = { 
-				ConsoTotal = { sidMap.Energy, "KWH" }, 
-				Watts = { sidMap.Energy, "Watts"}
-			}
-		   },
-	["28_0"] = { type = "DevUV", 
-			params = { 
-				Value = { sidMap.Light, "CurrentLevel" }
-			}
-		   }
-}
+-- Return the minimum ISS device header
+local function buildDeviceDescription(id,nm,rm,tp)
+	local d = {}
+	d.id = tostring(id)
+	d.name = nm
+	d.room = tostring(rm)
+	d.type = tp
+	return d
+end
+-- Return an ISS parameter object
+local function buildDeviceParameter(k,v)
+	local p = {}
+	p.key = k
+	p.value = v
+	return p
+end
+-- Return an ISS paramters object
+local function buildDeviceParamtersObject(id,params)
+	local p_t = {}
+	local pid = 1
+	for key, prm_t in pairs(params) do
+		local val
+		if type(prm_t) == "string" then
+			val = prm_t
+		else
+			val = luup.variable_get(prm_t[1], prm_t[2], id)
+		end	
+		if val and val ~= "" then
+			p_t[pid] = buildDeviceParameter(key, val)
+			pid = pid + 1
+		end    
+	end    
+	return p_t
+end
+-- Some special devices we do at schema level
+local schemaMap = {}
+-- Add a definition to the devMap table
+local function devSchema_Insert(idx, typ, par, act, par_f, act_f)
+	schemaMap[idx] = {}
+	schemaMap[idx].type = typ
+	if par then schemaMap[idx].params = par end
+	if act then schemaMap[idx].actions = act end
+	if par_f then schemaMap[idx].params_func = par_f end
+	if act_f then schemaMap[idx].actions_func = act_f end
+end    
+devSchema_Insert(SCHEMAS.SM_Gas, "DevGenericSensor", 
+				 { Value = { SIDS.SM_Gas, "Flow" }, defaultIcon = "https://raw.githubusercontent.com/reneboer/openLuup-ImperiHome/master/gas.png", unit = "l/h"})
+devSchema_Insert(SCHEMAS.Harmony, "DevMultiSwitch", nil, nil, 
+				function(id, issType)
+					local p_t = {}
+					local curActDesc = ""
+					local choices = ""
+					local curActID = luup.variable_get(SIDS.Harmony, "CurrentActivityID", id)
+					for bn = 1,25 do
+						local actDesc = luup.variable_get(SIDS.Harmony, "ActivityDesc"..bn, id)
+						local actID = luup.variable_get(SIDS.Harmony, "ActivityID"..bn, id)
+						if actDesc and actDesc ~= "" then
+							choices = choices ..actDesc .. ","
+							if actID == curActID then curActDesc = actDesc end
+						end    
+					end
+					p_t[1] = buildDeviceParameter("defaultIcon", "https://raw.githubusercontent.com/reneboer/vera-Harmony-Hub/master/icons/Harmony.png")
+					if curActDesc ~= "" then p_t[2] = buildDeviceParameter("Value", curActDesc) end	
+					if choices ~= "" then p_t[3] = buildDeviceParameter("Choices", choices:sub(1, -2)) end	
+					return p_t
+				end,
+				function(id, action, param, issType)
+					local a_t = {}
+					local param = param or ""
+					if action == "setChoice" and param ~= "" then
+						for bn = 1,25 do
+							local actDesc = luup.variable_get(SIDS.Harmony, "ActivityDesc"..bn, id)
+							if actDesc == param then 
+								local actID = luup.variable_get(SIDS.Harmony, "ActivityID"..bn, id)
+								a_t[1] = SIDS.Harmony
+								a_t[2] = "StartActivity"
+								a_t[3] = "newActivityID"
+								a_t[4] = actID
+							end	
+						end
+					end	
+					return a_t
+				end)
+devSchema_Insert(SCHEMAS.HarmonyDev, "DevMultiSwitch", nil, nil,
+				function(id, issType)
+					local p_t = {}
+					local choices = ""
+					for bn = 1,25 do
+						local actDesc = luup.variable_get(SIDS.HarmonyDev, "CommandDesc"..bn, id)
+						local actID = luup.variable_get(SIDS.HarmonyDev, "CommandID"..bn, id)
+						if actDesc and actDesc ~= "" then choices = choices ..actDesc .. "," end    
+					end
+					p_t[1] = buildDeviceParameter("defaultIcon", "https://raw.githubusercontent.com/reneboer/vera-Harmony-Hub/master/icons/Harmony.png")
+					if choices ~= "" then p_t[2] = buildDeviceParameter("Choices", choices:sub(1, -2)) end	
+					return p_t
+				end,
+				function(id, action, param, issType)
+					local a_t = {}
+					local param = param or ""
+					if action == "setChoice" and param ~= "" then
+						for bn = 1,25 do
+							local actDesc = luup.variable_get(SIDS.HarmonyDev, "CommandDesc"..bn, id)
+							if actDesc == param then 
+								local actID = luup.variable_get(SIDS.HarmonyDev, "CommandID"..bn, id)
+								a_t[1] = SIDS.HarmonyDev
+								a_t[2] = "SendDeviceCommand"
+								a_t[3] = "Command"
+								a_t[4] = actID
+							end	
+						end
+					end	
+					return a_t
+				end)
+devSchema_Insert(SCHEMAS.MSwitch,"DevMultiSwitch", {}, {})
+
+-- mapping between ImperiHome ISS and Vera device category and subcategory_num
+local devMap = {}
+-- Add a definition to the devMap table
+local function devMap_Insert(cat, sub_cat, typ, par, act)
+    if type(cat) == "number" and type(sub_cat) == "number" then
+        local idx = cat.."_"..sub_cat
+        devMap[idx] = {}
+        devMap[idx].type = typ
+        if par then devMap[idx].params = par end
+        if act then devMap[idx].actions = act end
+    end
+end    
+devMap_Insert(2,0, "DevDimmer", { Status = { SIDS.Switch, "Status" }, Level = { SIDS.Dimmer, "LoadLevelStatus" }, Energy = { SIDS.Energy, "Watts"}},
+								{ ["setLevel"] = { SIDS.Dimmer, "SetLoadLevelTarget", "newLoadlevelTarget" }}	)
+devMap_Insert(3,0, "DevSwitch", { Status = { SIDS.Switch, "Status" }, Energy = { SIDS.Energy, "Watts"}},
+								{ ["setStatus"] = { SIDS.Switch, "SetTarget", "newTarget" }} )
+local sensParams = { Armed = { SIDS.Sensor, "Armed" }, Tripped = { SIDS.Sensor, "Tripped"},	lasttrip = { SIDS.Sensor, "LastTrip"}, armable = "1" }
+local sensActions = { ["setArmed"] = { SIDS.Sensor, "SetArmed", "newArmedValue" }	}			
+devMap_Insert(4,0, "DevDoor", sensParams,sensActions)
+devMap_Insert(4,1, "DevDoor", sensParams,sensActions)
+devMap_Insert(4,2, "DevFlood", sensParams,sensActions)
+devMap_Insert(4,3, "DevMotion", sensParams,sensActions)
+devMap_Insert(4,3, "DevSmoke", sensParams,sensActions)
+devMap_Insert(4,3, "DevCO2Alert", sensParams,sensActions)
+devMap_Insert(12,0, "DevGenericSensor", { Value = { SIDS.Generic, "CurrentLevel" }})
+devMap_Insert(16,0, "DevHygrometry", { Value = { SIDS.Humidity, "CurrentLevel" }})
+devMap_Insert(17,0, "DevTemperature", {	Value = { SIDS.Temp, "CurrentTemperature" }})
+devMap_Insert(18,0, "DevLuminosity", { Value = { SIDS.Light, "CurrentLevel" }})
+devMap_Insert(21,0, "DevElectricity", {	ConsoTotal = { SIDS.Energy, "KWH" }, Watts = { SIDS.Energy, "Watts"}})
+devMap_Insert(28,0, "DevUV", { Value = { SIDS.Light, "CurrentLevel" }})
+
 
 -- Get information on the openLuup system
 function ISS_GetSystem()
@@ -217,19 +243,14 @@ function ISS_GetRooms()
 	res.success = true
 	return res
 end
-local function buildDeviceDescription(id,nm,rm,tp)
-	local d = {}
-	d.id = tostring(id)
-	d.name = nm
-	d.room = tostring(rm)
-	d.type = tp
-	return d
-end
-local function buildDeviceParameter(k,v)
-	p = {}
-	p.key = k
-	p.value = v
-	return p
+-- Search the schemaMap table for the matching schema. Allows for devices like Harmony Hub and MultiSwitch
+local function findSchema(schema)
+    for sk, dev in pairs(schemaMap) do
+        local m,_= sk:gsub("%-", "%%%-")
+        local mtch = schema:match(m)
+        if (mtch ~= nil) then return true, dev end
+    end
+    return false
 end
 function ISS_GetDevices()
 	local did = 1
@@ -239,74 +260,23 @@ function ISS_GetDevices()
 		-- Ignore hidden or invisible devices and those created by VeraBridge.
 		if not (dev.hidden or dev.invisible or id >= 10000) then
 			-- For special types we want to map based on schema
-			if string.sub(dev.device_type, 1, string.len(schemaMap.HarmonyDev)) == schemaMap.HarmonyDev then
---[[  Not yet supported, but catch here else it matches with schemaMap.Harmony
-				local d = buildDeviceDescription(id, dev.description, dev.room_num, "DevMultiSwitch")
-				d.params = {}
-				local choices = ""
-				for bn = 1,25 do
-					local actDesc = luup.variable_get(sidMap.HarmonyDev, "CommandDesc"..bn, id)
-					local actID = luup.variable_get(sidMap.HarmonyDev, "CommandID"..bn, id)
-					if actDesc and actDesc ~= "" then
-						choices = choices ..actDesc .. ","
-					end    
+			local fnd, issType = findSchema(dev.device_type)
+			if fnd then
+				local d = buildDeviceDescription(id, dev.description, dev.room_num, issType.type)
+				if issType.params_func then 
+					d.params = issType.params_func(id, issType) 
+				else	
+					-- We just have simple paramters
+					d.params = buildDeviceParamtersObject(id, issType.params)
 				end
-				if choices ~= "" then
-					d.params[1] = buildDeviceParameter("Choices", choices)
-				end	
-				res.devices[did] = d
-				did = did + 1
-]]				
-			elseif string.sub(dev.device_type, 1, string.len(schemaMap.Harmony)) == schemaMap.Harmony then
-				local d = buildDeviceDescription(id, dev.description, dev.room_num, "DevMultiSwitch")
-				d.params = {}
-				local curActDesc = ""
-				local choices = ""
-				local curActID = luup.variable_get(sidMap.Harmony, "CurrentActivityID", id)
-				for bn = 1,25 do
-					local actDesc = luup.variable_get(sidMap.Harmony, "ActivityDesc"..bn, id)
-					local actID = luup.variable_get(sidMap.Harmony, "ActivityID"..bn, id)
-					if actDesc and actDesc ~= "" then
-						choices = choices ..actDesc .. ","
-						if actID == curActID then curActDesc = actDesc end
-					end    
-				end
-				d.params[1] = buildDeviceParameter("defaultIcon", "https://raw.githubusercontent.com/reneboer/vera-Harmony-Hub/master/icons/Harmony.png")
-				if curActDesc ~= "" then
-					d.params[2] = buildDeviceParameter("Value", curActDesc)
-				end	
-				if choices ~= "" then
-					d.params[3] = buildDeviceParameter("Choices", choices)
-				end	
-				res.devices[did] = d
-				did = did + 1
-			elseif dev.device_type == schemaMap.SM_Gas then
-				local d = buildDeviceDescription(id, dev.description, dev.room_num, "DevGenericSensor")
-				d.params = {}
-				d.params[1] = buildDeviceParameter("defaultIcon", "https://raw.githubusercontent.com/reneboer/openLuup-ImperiHome/master/gas.png")
-				d.params[2] = buildDeviceParameter("Value", luup.variable_get(sidMap.SM_Gas, "Flow", id))
-				d.params[3] = buildDeviceParameter("unit", "l/h")
 				res.devices[did] = d
 				did = did + 1
 			else
-				local issType = devMap[dev.category_num..'_'..dev.subcategory_num]
+				issType = devMap[dev.category_num..'_'..dev.subcategory_num]
 				if not issType then issType = devMap[dev.category_num..'_0'] end
 				if issType then
 					local d = buildDeviceDescription(id, dev.description, dev.room_num, issType.type)
-					d.params = {}
-					local pid = 1
-					for key, prm_t in pairs(issType.params) do
-						local val
-						if type(prm_t) == "string" then
-							val = prm_t
-						else
-							val = luup.variable_get(prm_t[1], prm_t[2], id)
-						end	
-						if val and val ~= "" then
-							d.params[pid] = buildDeviceParameter(key, val)
-							pid = pid + 1
-						end    
-					end    
+					d.params = buildDeviceParamtersObject(id, issType.params)
 					res.devices[did] = d
 					did = did + 1
 				end
@@ -328,13 +298,22 @@ function ISS_SendCommand(devid, action, param)
 	local id = tonumber(devid) or 0
 	local dev = luup.devices[id]
 	if dev then
-		local issType = devMap[dev.category_num..'_'..dev.subcategory_num]
-		if not issType then issType = devMap[dev.category_num..'_0'] end
+		local fnd, issType = findSchema(dev.device_type)
+		if not fnd then
+			issType = devMap[dev.category_num..'_'..dev.subcategory_num]
+			if not issType then issType = devMap[dev.category_num..'_0'] end
+		end	
 		if issType then
-			local act_t = issType.actions[action]
+			local act_t 
+			if issType.actions_func then
+				act_t = issType.actions_func(id, action, param, issType)
+			else
+				act_t = issType.actions[action]
+				act_t[4] = tostring(param) or ""
+			end	
 			if act_t then
 				local prm = {}
-				if param and act_t[3] then prm[act_t[3]] = tostring(param) end
+				if act_t[3] and (act_t[4] ~= "") then prm[act_t[3]] = act_t[4] end
 				luup.call_action(act_t[1],act_t[2],prm,id)
 				res.success = true
 				res.errormsg=""
@@ -361,11 +340,9 @@ function run(wsapi_env)
 	local headers = {["Content-Type"] = "text/plain"}
 	local status, return_content, issRes, pcstat
 	-- Find right function to ISS API
-_log(wsapi_env.QUERY_STRING)	
 	local _, func_st = string.find(wsapi_env.QUERY_STRING, "query=", 1, true)
 	if func_st then
 		local func = string.sub(wsapi_env.QUERY_STRING, 8)
-_log(func)	
 		if func == "system" then
 			pcstat, issRes = pcall(ISS_GetSystem)
 		elseif func == "rooms" then
